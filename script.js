4b5e5c45d8cf947d64a37dd56c62623f76802dba
GMissaka <GMissaka@users.noreply.github.com>
2024-07-11 16:03:53 -0500

    Update script.js

--- script.js
+++ script.js
@@ -107,8 +107,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 draftLettersDiv.appendChild(button);
             });
             document.getElementById('game-board').classList.remove('hidden');
-        };
-        else {
+        } else {
             finishLetterSelection();
         }
     }
